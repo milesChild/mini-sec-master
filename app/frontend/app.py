43bff5596938103e3c2dc9236739562f1bf32fb3
Mihir Joshi <mnj2122@columbia.edu>
2025-06-02 16:20:30 -0500

    fixed ruff linting for docker version

--- app/frontend/app.py
+++ app/frontend/app.py
@@ -1,19 +1,19 @@
-import sys
 import os
-sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+import sys
+
+import streamlit as st
+from api.fmp_client import get_company_data_for_ticker
+
 """
 Streamlit UI for our mini security master.
 """
 
-import streamlit as st
-
-from api.fmp_client import get_company_data_for_ticker
-
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 st.title("📈 Mini Security Master")
 
 # ticker = st.text_input("Enter a stock ticker", value="AAPL", max_chars=10) . . .
 
-aapl_data = get_company_data_for_ticker("AAPL")
+aapl_data = get_company_data_for_ticker("MSFT")
 
 st.write(aapl_data)
 
